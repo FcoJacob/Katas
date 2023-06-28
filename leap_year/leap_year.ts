2fc08c910ad7404a58e9a99c56e2f248f662c2eb
fsarmiento <fsarmiento@aidacanarias.com>
2023-06-28 08:45:37 +0100

    :white_check_mark:  should return true if it is divisible by 4 but not by 100

--- leap_year/leap_year.ts
+++ leap_year/leap_year.ts
@@ -1,7 +1,7 @@
 export default class LeapYear {
     public isLeapYear(year: number): boolean {
         let isLeap = false;
-        if (year % 400 === 0) {
+        if (year % 400 === 0 || year % 4 === 0) {
             isLeap = true;
         }
         return isLeap;
