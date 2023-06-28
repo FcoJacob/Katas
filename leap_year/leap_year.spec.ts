13fc2b326e413b4d8df002cdcd27f23a4bcbc1a6
fsarmiento <fsarmiento@aidacanarias.com>
2023-06-28 08:39:25 +0100

    :white_check_mark: should return false if it is divisible by 100 but not by 400

--- leap_year/leap_year.spec.ts
+++ leap_year/leap_year.spec.ts
@@ -9,6 +9,6 @@ describe('leap_year_kata', () => {
 
     it('should return false if it is divisible by 100 but not by 400', () => {
         const isLeapYear = new LeapYear().isLeapYear(1400);
-        expect(isLeapYear).toBe(true);
+        expect(isLeapYear).toBe(false);
     });
 })
--- leap_year/leap_year.ts
+++ leap_year/leap_year.ts
@@ -1,5 +1,9 @@
 export default class LeapYear {
     public isLeapYear(year: number): boolean {
-        return year % 400 === 0;
+        let isLeap = false;
+        if (year % 400 === 0) {
+            isLeap = true;
+        }
+        return isLeap;
     }
 }
