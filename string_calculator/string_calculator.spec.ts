9cb36daee9a183049c00b98423d87e2fff807557
fsarmiento <fsarmiento@aidacanarias.com>
2023-07-05 08:05:42 +0100

    :test_tube: should throw a "negatives not alloweed" exception, when entering negative values

--- string_calculator/string_calculator.spec.ts
+++ string_calculator/string_calculator.spec.ts
@@ -4,26 +4,43 @@ import StringCalculator from "./string_calculator";
 describe('StringCalculator', () => {
     it('should return 0, when string is empty', () => {
         const calculator = new StringCalculator("");
+
         expect(calculator.add()).toBe(0);
     });
+
     it('should return value as number, when string is length 1', () => {
         const calculator = new StringCalculator("1");
+
         expect(calculator.add()).toBe(1);
     });
+
     it('should return the sum of the values, when receiving a string with comma-separated numbers', () => {
         const calculator = new StringCalculator("1,2");
+
         expect(calculator.add()).toBe(3);
     });
+
     it('should return the sum of all values, when receiving a string with comma-separated numbers', () => {
         const calculator = new StringCalculator("1,2,3,4");
+
         expect(calculator.add()).toBe(10);
     });
+
     it('should return the sum of all values, when receiving a string with trolley-separator numbers', () => {
         const calculator = new StringCalculator("1\n2\n3\n4");
+
         expect(calculator.add()).toBe(10);
     });
+
     it('should support different separators, if the string starts with "//[delimiter]\n..."', () => {
         const calculator = new StringCalculator("//[;]\n1;2;3;4");
+
         expect(calculator.add()).toBe(10);
+    });
+
+    it('should throw a "negatives not alloweed" exception, when entering negative values', () => {
+        const calculator = new StringCalculator("//[;]\n-4");
+
+        expect(calculator.add()).toThrow('negatives not allowed');
     });
 })
