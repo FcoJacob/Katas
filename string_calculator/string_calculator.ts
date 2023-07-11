e680f46773cdc21d31af5ccb858c107791244fc2
fsarmiento <fsarmiento@aidacanarias.com>
2023-07-11 08:35:09 +0100

    :recycle: string_calculator

--- string_calculator/string_calculator.ts
+++ string_calculator/string_calculator.ts
@@ -1,28 +1,39 @@
 export default class StringCalculator {
-    private readonly value: string;
+    private readonly userEnteredString: string;
 
     constructor(value: string) {
-        this.value = value;
+        this.userEnteredString = value;
     }
 
     public add(): number {
-        if (this.value === '') return 0;
-        if (this.value.length === 1) return parseInt(this.value, 10);
-        if (this.value.includes('\n')) {
-            let values: string[] = [];
-            const lines: string[] = this.value.split('\n');
-            if (lines[0].includes('//[')) {
-                const delimiter: string = lines[0].split('//[')[1].split(']')[0];
-                values = lines[1].split(delimiter);
-                const anyValuesAreNegative: boolean = values.every((value: string): boolean => parseInt(value) < 0)
+        if (this.userEnteredString === '') return 0;
+
+        if (this.userEnteredString.length === 1) return parseInt(this.userEnteredString, 10);
+
+        if (this.userEnteredString.includes('\n')) {
+            let valuesToOperate: string[] = [];
+            let delimiter: string = '';
+            const delimiterAndValues: string[] = this.userEnteredString.split('\n');
+            const delimiterExtraction: string = delimiterAndValues[0];
+            const valuesExtraction: string = delimiterAndValues[1];
+
+            if (delimiterExtraction.includes('//[')) {
+                delimiter = delimiterExtraction.split('//[')[1].split(']')[0];
+                valuesToOperate = valuesExtraction.split(delimiter);
+
+                const anyValuesAreNegative: boolean = valuesToOperate.every((value: string): boolean => parseInt(value) < 0)
                 if (anyValuesAreNegative) {
-                    throw new Error(`negatives not allowed, (${values})`);
+                    throw new Error(`negatives not allowed, (${valuesToOperate})`);
                 }
-                return values.map(Number).reduce((a: number, b:number) => a + b, 0);
+
+                return valuesToOperate.map(Number).reduce((a: number, b: number) => a + b, 0);
             }
-            return this.value.split('\n').map(Number).reduce((a: number, b:number) => a + b, 0)
+
+            return this.userEnteredString.split('\n').map(Number).reduce((a: number, b:number) => a + b, 0)
         }
-        if (this.value.includes(',')) return this.value.split(',').map(Number).reduce((a: number, b:number) => a + b, 0);
+
+        if (this.userEnteredString.includes(',')) return this.userEnteredString.split(',').map(Number).reduce((a: number, b:number) => a + b, 0);
+
         return 0
     }
 }
